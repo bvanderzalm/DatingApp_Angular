8938bb1a6a5b8a1d72cd4122cc25a110d673336a
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-21 21:09:47 -0400

    Added member and photo model with good type safety with typescript. Also utilizied environment files so in testing it'll use localhost and production it'll you something else. No longer hardcoded.

--- DatingApp/client/src/app/_models/user.ts
+++ DatingApp/client/src/app/_models/user.ts
@@ -2,31 +2,3 @@ export interface User {
     username: string;
     token: string;
 }
-
-// Typescript basics (Lecture 102)
-
-let data: number | string = 42;     // This variable can either be a string or number.
-
-data = "10";
-data = 15;      // Both of these don't cause any issues.
-
-interface Car {
-    color: string;
-    model: string;
-    topSpeed?: number;  // adding the question mark makes it optional
-}
-
-const car1: Car = {
-    color: 'blue',
-    model: 'BMW'
-}
-
-const car2: Car = {
-    color: 'red',
-    model: 'Mercedes',
-    topSpeed: 100
-}
-
-const multiply = (x: number,y: number): void => {
-    x * y;
-}
