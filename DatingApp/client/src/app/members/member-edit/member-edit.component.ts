869ba703b6b6b3f7aac197b17c05becfa9f34bbd
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-24 12:35:40 -0400

    Updated angular form, the save changes and blue popup bar only appear when changes have bee made to the form. Everything is working and ready except for the actual update with API to the database.

--- DatingApp/client/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp/client/src/app/members/member-edit/member-edit.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { MembersService } from 'src/app/_services/members.service';
 import { AccountService } from 'src/app/_services/account.service';
 import { take } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -11,10 +13,13 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
 
-  constructor(private accountService: AccountService, private memberService: MembersService) {
+  constructor(private accountService: AccountService, private memberService: MembersService, 
+    private toastr: ToastrService) {
+
     // Get user out of observable. Now our user will have the current user from our account service
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
   }
@@ -27,6 +32,15 @@ export class MemberEditComponent implements OnInit {
     this.memberService.getMember(this.user.username).subscribe(member => {
       this.member = member;
     })
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    
+    // This will reset the form to what the page was before. So Save changes button will be disabled and
+    // the popup will disappear.
+    this.editForm.reset(this.member);
   }
 
 }
