e297754d27b765f25f3d7613f750fc0637b24e84
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-24 22:02:59 -0400

    End of Section 10. Updated memberService to store loaded members into an array. So when you switch to different pages it won't load or call an API request everytime.

--- DatingApp/client/src/app/_services/members.service.ts
+++ DatingApp/client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -9,18 +11,38 @@ import { Member } from '../_models/member';
 export class MembersService {
   baseUrl = environment.apiUrl;
 
+  // This service is in use until we shut it down, so why not store the members info.
+  members: Member[] = [];
+
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    // If we have already loaded the members from the database, no need to load it again, just return it from our service.
+    if (this.members.length > 0) 
+      return of(this.members);
+    
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        // Update the array stored here in the service. First find the index of the member that is being updated and update the array to that.
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 
 }
