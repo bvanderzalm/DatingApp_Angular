74fc5396d9b4d3800f0e3ebe3158c77611b3ebac
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-26 21:31:41 -0400

    Added pagination in the API. Successful in Postman.

--- DatingApp/API/Data/UserRepository.cs
+++ DatingApp/API/Data/UserRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -39,11 +40,13 @@ namespace API.Data
                 // }).SingleOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
+            var query =  _context.Users
                 .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .ToListAsync();
+                .AsNoTracking();    // only read, nothing else. make it more efficient
+            
+            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
