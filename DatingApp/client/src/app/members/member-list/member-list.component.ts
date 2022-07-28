1bd22729f2e47064e15ed7ad470bba9227cc107a
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-27 20:30:32 -0400

    Added bootstrap pages where user can now switch between pages.

--- DatingApp/client/src/app/members/member-list/member-list.component.ts
+++ DatingApp/client/src/app/members/member-list/member-list.component.ts
@@ -27,4 +27,10 @@ export class MemberListComponent implements OnInit {
       this.pagination = response.pagination;
     })
   }
+
+  // So if a page is changed on the html it will trigger this method based on the event we passed
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadMembers();
+  }
 }
