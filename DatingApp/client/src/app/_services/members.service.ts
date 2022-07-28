ecfa01ca1b10de9e7756a22103d41cc72853e4d0
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-27 22:40:14 -0400

    Added clientside sorting with Last Active and Newest Members.

--- DatingApp/client/src/app/_services/members.service.ts
+++ DatingApp/client/src/app/_services/members.service.ts
@@ -24,6 +24,7 @@ export class MembersService {
     params = params.append('minAge', userParams.minAge.toString());
     params = params.append('maxAge', userParams.maxAge.toString());
     params = params.append('gender', userParams.gender);
+    params = params.append('orderBy', userParams.orderBy);
     
     // When we are observing an response, we are getting the full response back.
     return this.getPaginatedResults<Member[]>(this.baseUrl + 'users', params);
