0fb76ffcef5d5fda58157e7d3cc63ab1d50f241f
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-31 11:29:45 -0400

    Added clientside online presence. Where if a user is logged in it will show a little green icon on their member card and member detail page. Icon will go away if they leave the site or logout.

--- DatingApp/client/src/app/_services/presence.service.ts
+++ DatingApp/client/src/app/_services/presence.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
@@ -11,6 +12,9 @@ import { User } from '../_models/user';
 export class PresenceService {
   hubUrl = environment.hubUrl;
   private hubConnection: HubConnection;
+  // String array of our online users
+  private onlineUsersSource = new BehaviorSubject<string[]>([]);
+  onlineUsers$ = this.onlineUsersSource.asObservable();
 
   constructor(private toastr: ToastrService) { }
 
@@ -32,6 +36,11 @@ export class PresenceService {
 
     this.hubConnection.on('UserIsOffline', username => {
       this.toastr.warning(username + ' has disconnected');
+    })
+
+    this.hubConnection.on('GetOnlineUsers', (usernames: string[]) => {
+      // Update behavior subject with our current list of online users.
+      this.onlineUsersSource.next(usernames);
     })
   }
 
