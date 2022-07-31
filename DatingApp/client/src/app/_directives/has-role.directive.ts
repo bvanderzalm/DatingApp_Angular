bece4ef8216e00c304acca62d540d551880571ee
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-30 23:17:36 -0400

    User can no longer see admin page link in nav bar when they are not an admin.

---
+++ DatingApp/client/src/app/_directives/has-role.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+// *ngIf and *ngFor are examples of default angular directives
+@Directive({
+  // Now this custom directive will be:
+  selector: '[appHasRole]'  //  *appHasRole='["Admin"]'
+})
+export class HasRoleDirective implements OnInit {
+  @Input() appHasRole: string[];
+  user: User;
+
+  constructor(private viewContainerRef: ViewContainerRef, private templateRef: TemplateRef<any>, 
+    private accountService: AccountService) {
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+        this.user = user;
+      })
+    }
+
+  ngOnInit(): void {
+    // Clear view if no roles
+    if (!this.user?.roles || this.user == null) {
+      this.viewContainerRef.clear();
+      return;
+    }
+
+    if (this.user?.roles.some(r => this.appHasRole.includes(r))) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }
+  }
+
+}
