509c8196a8f915e6ce3106ebd63f0da0d6dc66ea
Bradley Vanderzalm <bvan544@gmail.com>
2022-07-30 21:33:19 -0400

    Added roles, updated seed data and dropped database.

--- DatingApp/API/Data/Seed.cs
+++ DatingApp/API/Data/Seed.cs
@@ -12,7 +12,7 @@ namespace API.Data
     // This is used to populate our database/
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync())
                 return;
@@ -21,14 +21,34 @@ namespace API.Data
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
 
             if (users == null) return;
+
+            var roles = new List<AppRole>
+            {
+                new AppRole{Name = "Member"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Moderator"},
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
             
             // Loop through all users
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();
-
                 await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Member");
             }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
         }
     }
 }
